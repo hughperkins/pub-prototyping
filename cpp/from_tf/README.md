e71018ccfbf503db1d245ed1e7294ac16b71531b
Hugh Perkins <hughperkins@gmail.com>
2017-06-01 10:52:57 +0100

    testphilox.cpp

---
+++ cpp/from_tf/README.md
@@ -0,0 +1,3 @@
+The files in this directory were copied from tensorflow repository, https://github.com/tensorflow/tensorflow
+
+They are available under an Apache 2.0 license, see LICENSE ,contributors etc, in this directory
---
+++ cpp/testphilox.cpp
@@ -0,0 +1,28 @@
+#include "from_tf/philox_random.h"
+#include "tensorflow/core//lib/random/random_distributions.h"
+
+#include <iostream>
+#include <cassert>
+
+static void fill(tensorflow::random::PhiloxRandom rnd, uint32_t* output, int start, int limit) {
+   assert(start % 4 == 0);
+   assert(limit % 4 == 0);
+   rnd.Skip(start / 4);
+   for (int i = start; i < limit; i += 4) {
+     auto sample = rnd();
+     for(int j = 0; j < 4; j++) {
+        output[i + j] = sample[j];
+     }
+   }
+ }
+
+int main(int argc, char *argv[]) {
+    // tensorflow::random::PhiloxRandom rand(123);
+    tensorflow::random::PhiloxRandom rand(87654321, 123);
+    uint32_t numbers[12];
+    fill(rand, numbers, 0, 12);
+    for(int i=0; i < 12; i++) {
+        std::cout << i << ": " << numbers[i] << " " << tensorflow::random::Uint32ToFloat(numbers[i]) << std::endl;
+    }
+    return 0;
+}
