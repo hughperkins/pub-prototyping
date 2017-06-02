bc93e8ed5493eb206dd54100560625cc0be6fe36
Hugh Perkins <hughperkins@gmail.com>
2017-06-02 08:13:52 +0100

    add argparsecpp

--- cpp/testphilox.cpp
+++ cpp/testphilox.cpp
@@ -3,6 +3,8 @@
 #include "tensorflow/core/platform/types.h"
 #include "tensorflow/core//lib/random/random_distributions.h"
 
+#include "third_party/argparsecpp/argparsecpp.h"
+
 #include <iostream>
 #include <cassert>
 
@@ -18,13 +20,42 @@ static void fill(tensorflow::random::PhiloxRandom rnd, uint32_t* output, int sta
    }
  }
 
-int main(int argc, char *argv[]) {
+void uniform(int N) {
     // tensorflow::random::PhiloxRandom rand(123);
     tensorflow::random::PhiloxRandom rand(87654321, 123);
-    uint32_t numbers[12];
-    fill(rand, numbers, 0, 12);
-    for(int i=0; i < 12; i++) {
-        std::cout << i << ": " << numbers[i] << " " << tensorflow::random::Uint32ToFloat(numbers[i]) << std::endl;
+    uint32_t numbers[N];
+    fill(rand, numbers, 0, N);
+    for(int i=0; i < N; i++) {
+      std::cout << i << ": " << numbers[i] << " " << tensorflow::random::Uint32ToFloat(numbers[i]) << std::endl;
+    }
+}
+
+void normal(int N) {
+    // tensorflow::random::PhiloxRandom rand(123);
+    tensorflow::random::PhiloxRandom rand(87654321, 123);
+    uint32_t numbers[N];
+    fill(rand, numbers, 0, N);
+    for(int i=0; i < N; i++) {
+      std::cout << i << ": " << numbers[i] << " " << tensorflow::random::Uint32ToFloat(numbers[i]) << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int N = 0;
+    std::string operation = "";
+
+    argparsecpp::ArgumentParser parser;
+    parser.add_string_argument("--operation", &operation)->defaultValue("uniform")->help("[uniform|normal]");
+    parser.add_int_argument("--count", &N)->defaultValue(12)->help("how many numbers to generate");
+    if(!parser.parse_args(argc, argv)) {
+        return -1;
+    }
+    std::cout << "count: " << N << std::endl;
+
+    if(operation == "uniform") {
+      uniform(N);
+    } else if(operation == "normal") {
+      normal(N);
     }
     return 0;
 }
