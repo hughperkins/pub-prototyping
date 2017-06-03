0412927d34c69ddb1db79194b72c3c1b4cab048b
Hugh Perkins <hughperkins@gmail.com>
2017-06-03 12:05:21 +0100

    compiles, doesnt link

--- cpp/tf/test_cuda_device_array.cpp
+++ cpp/tf/test_cuda_device_array.cpp
@@ -34,27 +34,92 @@ public:
     }
     int value;
 };
+
 struct TensorShape {
     int64_t x;
 };
-class TensorReference {
+
+class AllocatorAttributes {
 public:
-    void Unref() const;
+    void set_on_host(bool v) {
+        this->on_host = v;
+    }
+    void set_gpu_compatible(bool v) {
+        this->gpu_compatible = v;
+    }
+    bool on_host = false;
+    bool gpu_compatible;
 };
+
 namespace perftools {
     namespace gputools {
-        class DeviceMemoryBase {
-        public:
+        struct DeviceMemoryBase {
+            float *mem;
+            uint64_t size;
         };
     }
 }
+
+class Tensor {
+public:
+    template<typename T>
+    Tensor flat() {
+        std::cout << "Tensor::flat()" << std::endl;
+        Tensor ret;
+        return ret;
+    }
+    bool IsInitialized() const;
+    float *data();
+    // Tensor data() {
+    //     std::cout << "Tensor::data()" << std::endl;
+    //     return 0;
+    // }
+};
+
+class TensorReference {
+public:
+    TensorReference(Tensor tensor) {
+        std::cout << "TensorReference::TensorReference()" << std::endl;
+    }
+    void Unref() const;
+};
+
+class Stream;
+class Device;
+
+class OpDeviceContext {
+public:
+    Stream *stream();
+};
+
+class OpKernelContext {
+public:
+    Status allocate_temp(DT<int8_t> dt, TensorShape tensorShape, Tensor *tensor, AllocatorAttributes attrib) {
+        std::cout << "OpKernelContext::allocate_temp(dt, shape, tensor, attrib)" << std::endl;
+        return Status::OK();
+    }
+    Status allocate_temp(DT<int8_t> dt, TensorShape tensorShape, Tensor *tensor) {
+        std::cout << "OpKernelContext::allocate_temp(dt, shape, tensor)" << std::endl;
+        return Status::OK();
+    }
+    OpDeviceContext *op_device_context() {
+        return &opDeviceContext;
+    }
+    Device *device();
+    OpDeviceContext opDeviceContext;
+};
+
 class Stream {
 public:
-    void ThenMemcpy(float *);
+    void ThenMemcpy(perftools::gputools::DeviceMemoryBase *, float *, uint32_t total_bytes);
 };
+    // stream->ThenMemcpy(&output_values_base,
+    //                    out_of_line_values_on_host_.flat<int8>().data(),
+    //                    total_bytes_);
+
 class EventMgr {
 public:
-    void ThenExecute(Stream &stream, std::function<void ()> fn);
+    void ThenExecute(Stream *stream, std::function<void ()> fn);
 };
 
 class GpuDeviceInfo {
@@ -67,60 +132,42 @@ public:
     }
     EventMgr *event_mgr;
 };
-class AllocatorAttributes {
-public:
-    void set_on_host(bool v) {
-        this->on_host = v;
-    }
-    void set_gpu_compatible(bool v) {
-        this->gpu_compatible = v;
-    }
-    bool on_host = false;
-    bool gpu_compatible;
-};
-class Tensor {
-public:
-    template<typename T>
-    Tensor flat() {
-        std::cout << "Tensor::flat()" << std::endl;
-        Tensor ret;
-        return ret;
-    }
-    bool IsInitialized() const;
-    float *data() {
-        std::cout << "Tensor::data()" << std::endl;
-        return 0;
-    }
-};
+
 class Device {
 public:
     GpuDeviceInfo *tensorflow_gpu_device_info();
 };
-class OpDeviceContext {
-public:
-    Stream *stream();
-};
-class OpKernelContext {
-public:
-    Status allocate_temp(DT<int8_t> dt, TensorShape tensorShape, Tensor *tensor, AllocatorAttributes attrib) {
-        std::cout << "OpKernelContext::allocate_temp()" << std::endl;
-        return Status::OK();
-    }
-    OpDeviceContext *op_device_context() {
-        return &opDeviceContext;
-    }
-    Device *device();
-    OpDeviceContext opDeviceContext;
-};
+
 #define TF_DISALLOW_COPY_AND_ASSIGN(x)
 
 #include "tf_files/cuda_device_array_gpu.h"
 #include "tf_files/cuda_device_array.h"
 
 int main(int argc, char *argv[]) {
+    // based loosely on tensorflow split_op.cc:
     OpKernelContext opKernelContext;
     int num_split = 4;
     tensorflow::CudaDeviceArrayOnHost<float *> ptrs(&opKernelContext, num_split);
     ptrs.Init();
+    ptrs.Finalize();
     return 0;
 }
+
+    // for (int i = 0; i < num_split; ++i) {
+    //   Tensor* result = nullptr;
+    //   OP_REQUIRES_OK(context,
+    //                  context->allocate_output(i, output_shape, &result));
+    //   std::cout << "  SplitOpGPU::Compute() i=" << i << " result=" << result << " num_split=" << num_split << std::endl;
+    //   std::cout << "  SplitOpGPU::Compute() result->flat<T>().data() " << result->flat<T>().data() << std::endl;
+    //   ptrs.Set(i, result->flat<T>().data());
+    // }
+    // if (prefix_dim_size * split_dim_output_size * suffix_dim_size == 0) {
+    //   return;
+    // }
+    // OP_REQUIRES_OK(context, ptrs.Finalize());
+
+    // std::cout << "  SplitOpGPU::Compute() call .Run(eigen_device, data=" << input.flat<T>().data() <<
+    //     " prefix_dim_size=" << prefix_dim_size << " split_dim_size=" << split_dim_size << " ptrs.data().size=" << ptrs.data().size << ")" << std::endl;
+    // SplitOpGPULaunch<T>().Run(context->eigen_device<GPUDevice>(),
+    //                           input.flat<T>().data(), prefix_dim_size,
+    //                           split_dim_size, suffix_dim_size, ptrs.data());
