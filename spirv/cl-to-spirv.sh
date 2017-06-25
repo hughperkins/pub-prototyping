e6319ef710b3861f28e6963db2b9b1f85729cbc0
Hugh Perkins <hughperkins@gmail.com>
2017-06-25 15:39:00 +0100

    first attempt at using spirv reader in cpp

--- spirv/cl-to-spirv.sh
+++ spirv/cl-to-spirv.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # designed to be run fomr subidrecotyr build
 
-source ~/git/marsohodmethod/activate
-source ~/spirv-tools/activate
+# source ~/git/marsohodmethod/activate
+# source ~/spirv-tools/activate
+export PATH=~/git-local/spirv/dist/bin:$PATH
+export PATH=~/git-local/spirv/spirv-tools/bin:$PATH
 
 # clang -cc1 -emit-llvm-bc -triple spir-unknown-unknown '' cl-spir-compile-options "" -include opencl_spir.h -o cl_kernel1.spv cl_kernel1.cl
 clang -cc1 -emit-spirv -triple spir-unknown-unknown -cl-std=CL1.2 -include opencl.h -x cl -o cl_kernel1.spv ../cl_kernel1.cl
