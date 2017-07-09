3f9ab0f1a002d5d35156883c9acdac6ec7706c6b
Hugh Perkins <hughperkins@gmail.com>
2017-07-09 23:41:34 +0100

    add non teacher prediction

--- papers/attention/mt_by_align_translate.py
+++ papers/attention/mt_by_align_translate.py
@@ -25,14 +25,6 @@ hidden_size = 128
 num_epochs = 16
 # N = 10
 
-class Rnn(nn.Module):
-    def __init__(self):
-        pass
-
-    def forward(self, x):
-        pass
-
-
 torch.manual_seed(123)
 np.random.seed(123)
 for n in range(N):
@@ -46,13 +38,12 @@ for n in range(N):
     for i in starred:
         chosen_words[i] = '*' + chosen_words[i]
     sentence = ' '.join(chosen_words)
-    # print('sentence [%s]' % sentence)
     target_words = []
     for i in starred:
         target_words.append(chosen_words[i][1:])
     target_sentence = ' '.join(target_words)
-    # print('target [%s]' % target_sentence)
     training.append({'input': sentence, 'target': target_sentence})
+
 print(training[0])
 if N > 1:
     print(training[1])
@@ -95,21 +86,8 @@ for i, example in enumerate(training):
         'input_encoded': encode_passage(example['input']),
         'target_encoded': encode_passage(example['target'])})
 
-# print(training_encoded[0])
-# print(training_encoded[1])
-
-# def run():
-
 V = len(char_by_idx)
 print('vocab size %s' % V)
-
-# W_enc = autograd.Variable(torch.rand(num_hidden, num_hidden) - 0.5)
-# b_enc = autograd.Variable(torch.rand(num_hidden) - 0.5) * 0.05
-
-# W_dec = autograd.Variable(torch.rand(num_hidden, num_hidden) - 0.5)
-# b_dec = autograd.Variable(torch.rand(num_hidden) - 0.5) * 0.05
-
-# embedding = autograd.Variable(torch.rand(V, num_hidden))
 
 # based very loosely on
 # http://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html#the-encoder
@@ -130,8 +108,7 @@ rnn_dec = nn.RNN(
     nonlinearity='tanh',
     bias=True,
 )
-# enc_opt = optim.Adam([embedding.parameters()] + [, lr=0.001)
-# dec_opt = optim.Adam(, lr=0.001)
+
 opt = optim.Adam(
     [p for p in embedding.parameters()] +
     [p for p in rnn_enc.parameters()] +
@@ -146,84 +123,73 @@ while True:
         target_encoded = ex['target_encoded']
         input_len = len(input_encoded)
         target_len = len(target_encoded)
-        # print('input_len', input_len)
+
         # encode first
-        initial_state = autograd.Variable(torch.zeros(1, 1, hidden_size))
-        # input_tensor = autograd.Variable(torch.zeros(input_len, 1))
-        input_tensor = autograd.Variable(torch.from_numpy(input_encoded).long().view(-1, 1))
-        # print('input_tensor.size()', input_tensor.size())
-        # input_tensor[:, 0, :] = input
-        # input_tensor = autograd.Variable(torch.Tensor(
-        #     input.reshape(input_len, 1, V)
-        # ))
-        input_embedded = embedding(input_tensor)
-        # print('input_embedded.size()', input_embedded.size())
-        # print('input_tensor.size()', input_tensor.size())
-        # print('initial_state.size()', initial_state.size())
-        out, hn = rnn_enc(input_embedded, initial_state)
-        # print('out.size()', out.size())
-        # print('hn.size()', hn.size())
-        # for i, v in enumerate(input):
-        # state = autograd.Variable(torch.zeros(num_hidden))
-        # for t, c in enumerate(input):
-        #     c_embedded = embedding[c]
-        #     state = c_embedded @ W_enc + b_enc
-        #     state = nn.functional.tanh(state)
-        # print('encoder output', state[:10].view(1, -1))
-        # if n >= 3:
-        #     sys.exit(0)
+        def encode(input_encoded):
+            initial_state = autograd.Variable(torch.zeros(1, 1, hidden_size))
+            input_tensor = autograd.Variable(torch.from_numpy(input_encoded).long().view(-1, 1))
+            input_embedded = embedding(input_tensor)
+            out, hn = rnn_enc(input_embedded, initial_state)
+            return hn
+
+        hn = encode(input_encoded)
 
         # now decode
         prev_c_encoded = autograd.Variable(
             torch.from_numpy(np.array([start_code], np.int32)).long().view(1, 1)
         )
-        # print('prev_c_encoded.size()', prev_c_encoded.size())
         loss = 0
         criterion = torch.nn.NLLLoss()
 
         output_sentence = ''
         for t, target_c_encoded in enumerate(target_encoded):
-            # print('target_c', target_c)
             # this is going to correspond approximately to
             # 'teacher forcing' in the seq2seq example
             # on the pytorch website
             prev_c_embedded = embedding(prev_c_encoded)
-            # print('prev_c_embedded.size()', prev_c_embedded.size())
             pred_c_embedded, hn = rnn_dec(prev_c_embedded, hn)
-            # print('pred_c_embedded.size()', pred_c_embedded.size())
-            # print('embedding.weight.size()', embedding.weight.size())
             pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding.weight.transpose(0, 1)
-            # print('pred_c.size()', pred_c.size())
             _, v = pred_c.max(-1)
-            # print('v', v.data[0][0])
-            # print(char_by_idx[v.data[0][0]])
             output_sentence += char_by_idx[v.data[0][0]]
             loss += criterion(pred_c, autograd.Variable(torch.LongTensor([target_c_encoded.item()])))
 
             prev_c_encoded = autograd.Variable(
                 torch.from_numpy(np.array([target_c_encoded], np.int32)).long().view(1, 1)
             )
-            # print('loss', loss)
-            # pred_c_embedded = rnn_dec()
-        # print('loss', loss)
+
+        def predict_on(input_encoded):
+            state = encode(input_encoded=input_encoded)
+            prev_c_encoded = autograd.Variable(
+                torch.from_numpy(np.array([start_code], np.int32)).long().view(1, 1)
+            )
+            output_sentence = ''
+            # for t, _ in enumerate(target_encoded):
+            for t in range(20):  # hardcode length for now...
+                # this is going to correspond approximately to
+                # 'teacher forcing' in the seq2seq example
+                # on the pytorch website
+                prev_c_embedded = embedding(prev_c_encoded)
+                pred_c_embedded, state = rnn_dec(prev_c_embedded, state)
+                pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding.weight.transpose(0, 1)
+                _, v = pred_c.max(-1)
+                v = v.data[0][0]
+                output_sentence += char_by_idx[v]
+
+                prev_c_encoded = autograd.Variable(
+                    torch.from_numpy(np.array([v], np.int32)).long().view(1, 1)
+                )
+            return output_sentence
+
         if n == 0:
             print(output_sentence)
+            print(predict_on(input_encoded=training_encoded[0]['input_encoded']))
 
         embedding.zero_grad()
         rnn_enc.zero_grad()
         rnn_dec.zero_grad()
         loss.backward()
         opt.step()
-        # print(dir(rnn_enc))
-        # print('rnn_enc.weight.grad', rnn_enc.weight_hh_l0.grad)
-        # sys.exit(0)
 
-        #     target_c_embedded = embedding[target_c]
-        #     state = c_embedded @ W_dec + b_dec
-        #     state = nn.functional.tanh(state)
-        # out = state @ embedding.transpose(0, 1)
-        # print('out.shape', out.shape)
-        # sys.exit(0)
     epoch += 1
 
 # if __name__ == '__main__':
