6b7d548a9a18b6aa465b02e5a9ea3d9cad42726f
Hugh Perkins <hughperkins@gmail.com>
2017-07-14 09:59:30 +0100

    try to make more idiomatic pytorch

--- papers/attention/seq2seq_noattention_trainbyparts.py
+++ papers/attention/seq2seq_noattention_trainbyparts.py
@@ -15,16 +15,11 @@ import encoding
 import data_anki as data
 
 
-# try normal rnn first
-# input: "this is a *test of *some stuff"
-# target: "test some"
-# (basically, we want any word that was prefixed by '*')
-
 N = 100
 # N = 8
 N = 16
 max_sentence_len = 10
-# N = 4
+N = 4
 print_every = 2
 hidden_size = 16
 # hidden_size = 1024
@@ -50,32 +45,59 @@ print('vocab size %s' % V)
 
 torch.manual_seed(123)
 np.random.seed(123)
-embedding = nn.Embedding(V, hidden_size)
-print('V', V, 'hidden_size', hidden_size)
-rnn_enc = nn.RNN(
-    input_size=hidden_size,
-    hidden_size=hidden_size,
-    num_layers=1,
-    nonlinearity='tanh',
-    bias=True,
-)
-rnn_dec = nn.RNN(
-    input_size=hidden_size,
-    hidden_size=hidden_size,
-    num_layers=1,
-    nonlinearity='tanh',
-    bias=True,
-)
+
+
+class Encoder(nn.Module):
+    def __init__(self, embedding):
+        super().__init__()
+        self.input_size = embedding.weight.size()[0]
+        self.hidden_size = embedding.weight.size()[1]
+        self.embedding = embedding
+        self.rnn_enc = nn.RNN(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=1,
+            nonlinearity='tanh'
+        )
+
+    def forward(self, x, state):
+        x = self.embedding(x)
+        x, state = self.rnn_enc(x, state)
+        return x, state
+
+
+class Decoder(nn.Module):
+    def __init__(self, embedding):
+        super().__init__()
+        self.input_size = embedding.weight.size()[0]
+        self.hidden_size = embedding.weight.size()[1]
+        self.embedding = embedding
+        self.rnn_dec = nn.RNN(
+            input_size=self.hidden_size,
+            hidden_size=self.hidden_size,
+            num_layers=1,
+            nonlinearity='tanh'
+        )
+
+    def forward(self, x, state):
+        x = self.embedding(x)
+        x, state = self.rnn_dec(x, state)
+        return x, state
+
 
 optimizer_fn = optim.Adam
 # optimizer_fn = optim.SGD
-parameters = (
-    [p for p in embedding.parameters()] +
-    [p for p in rnn_enc.parameters()]
-    + [p for p in rnn_dec.parameters()])
 
-opt = optimizer_fn(
-    parameters, lr=0.001)
+embedding = nn.Embedding(V, hidden_size)
+encoder = Encoder(embedding=embedding)
+decoder = Decoder(embedding=embedding)
+embedding_matrix = embedding.weight
+
+parameters = (
+    set(encoder.parameters()) |
+    set(decoder.parameters()) |
+    set(embedding.parameters()))
+opt = optimizer_fn(parameters, lr=0.001)
 
 epoch = 0
 while True:
@@ -92,83 +114,74 @@ while True:
         loss = 0
         criterion = torch.nn.NLLLoss()
 
-        # encode first
-        def encode(input_encoded):
+        # encode
+        def encode(input_encoded, state):
             global encoder_debug
             enc_loss = 0
-            initial_state = autograd.Variable(torch.zeros(1, 1, hidden_size))
-            prev_c_encoded = autograd.Variable(
-                torch.from_numpy(np.array([encoding.start_code], np.int32)).long().view(1, 1)
-            )
-            state = initial_state
+            prev_c = encoding.start_code
             input_sentence_verify = ''
             sentence = ''
             # [1:] is to cut off the start token
             # [:-1] is to cut off end token too :-)
-            for t, input_c_encoded in enumerate(input_encoded[1:]):
-                input_sentence_verify += encoding.char_by_idx[input_c_encoded]
-                prev_c_embedded = embedding(prev_c_encoded)
-                pred_c_embedded, state = rnn_enc(prev_c_embedded, state)
-                pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding.weight.transpose(0, 1)
+            for t, input_c in enumerate(input_encoded[1:]):
+                input_c = input_c.item()
+                input_sentence_verify += encoding.char_by_idx[input_c]
+                pred_c_embedded, state = encoder(autograd.Variable(torch.LongTensor([[prev_c]])), state)
+                pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding_matrix.transpose(0, 1)
                 _, v = pred_c.max(-1)
                 v = v.data[0][0]
                 sentence += encoding.char_by_idx[v]
-                if teacher_forcing or True:
-                    enc_loss += criterion(pred_c, autograd.Variable(torch.LongTensor([input_c_encoded.item()])))
-                prev_c_encoded = autograd.Variable(
-                    torch.from_numpy(np.array([input_c_encoded], np.int32)).long().view(1, 1)
-                )
+                # want to force encoder to build language model a bit faster than
+                # if it has to wait only for gradient from decoder:
+                enc_loss += criterion(pred_c, autograd.Variable(
+                    torch.LongTensor([input_c])))
+                prev_c = input_c
             if n <= 4 and epoch % print_every == 0:
                 if n == 0:
                     encoder_debug += 'epoch %s encoder:\n' % epoch
                 encoder_debug += '    [%s] => [%s]\n' % (input_sentence_verify, sentence)
             return state, enc_loss
 
-        state, enc_loss = encode(input_encoded)
+        state = autograd.Variable(torch.zeros(1, 1, hidden_size))
+        state, enc_loss = encode(input_encoded, state)
         loss += enc_loss
 
-        # now decode
+        # decode
         if True:
-            prev_c_encoded = autograd.Variable(
-                torch.from_numpy(np.array([encoding.start_code], np.int32)).long().view(1, 1)
-            )
+            prev_c = encoding.start_code
 
             output_sentence = ''
-            for t, target_c_encoded in enumerate(target_encoded[1:]):
-                # this is going to correspond approximately to
-                # 'teacher forcing' in the seq2seq example
-                # on the pytorch website
-                prev_c_embedded = embedding(prev_c_encoded)
-                pred_c_embedded, state = rnn_dec(prev_c_embedded, state)
-                pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding.weight.transpose(0, 1)
+            for t, target_c in enumerate(target_encoded[1:]):
+                target_c = target_c.item()
+
+                pred_c_embedded, state = decoder(
+                    autograd.Variable(torch.LongTensor([[prev_c]])), state)
+                pred_c = pred_c_embedded.view(-1, hidden_size) @ embedding_matrix.transpose(0, 1)
                 _, v = pred_c.max(-1)
                 v = v.data[0][0]
                 output_sentence += encoding.char_by_idx[v]
-                loss += criterion(pred_c, autograd.Variable(torch.LongTensor([target_c_encoded.item()])))
+                loss += criterion(pred_c, autograd.Variable(torch.LongTensor(
+                    [target_c])))
 
                 if teacher_forcing:
-                    prev_c_encoded = autograd.Variable(
-                        torch.from_numpy(np.array([target_c_encoded], np.int32)).long().view(1, 1)
-                    )
+                    prev_c = target_c
                 else:
                     # if we're already wrong, let's just abandon...
-                    if target_c_encoded != v:
+                    if target_c != v:
                         break
-                    prev_c_encoded = autograd.Variable(
-                        torch.from_numpy(np.array([v], np.int32)).long().view(1, 1)
-                    )
+                    prev_c = v
             if n <= 1 and epoch % print_every == 0:
                 if n == 0:
                     decoder_debug += 'epoch %s decoder:\n' % epoch
                 if not teacher_forcing:
-                    decoder_debug += '    [%s] => [%s] [%s]\n' % (ex['input'], ex['target'], output_sentence)
+                    decoder_debug += '    [%s] => [%s] [%s]\n' % (
+                        ex['input'], ex['target'], output_sentence)
         embedding.zero_grad()
-        rnn_enc.zero_grad()
-        rnn_dec.zero_grad()
+        encoder.zero_grad()
+        decoder.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm(parameters, 4.0)
         opt.step()
-
 
     if encoder_debug != '':
         print(encoder_debug)
